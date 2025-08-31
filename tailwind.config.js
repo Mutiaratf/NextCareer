d09fa2a53e56d76d7089d51e96821ba93b23b660
Mutiara Tari Fadilah <123176024+Mutiaratf@users.noreply.github.com>
2025-08-31 12:00:38 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563eb', // biru modern
+        secondary: '#9333ea', // ungu
+        accent: '#f59e0b',   // kuning
+      }
+    },
+  },
+  plugins: [],
+}
